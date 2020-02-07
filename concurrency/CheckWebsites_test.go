019b7049212da49ca32ce43013f79820344f0890
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-02-07 23:34:48 +0700

    Add concurrency section

---
+++ concurrency/CheckWebsites_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func mockWebsiteChecker(url string) bool {
+	if url == "http://mock.mock" {
+		return false
+	}
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://example.com",
+		"http://mock.mock",
+	}
+
+	want := map[string]bool{
+		"http://google.com": true,
+		"http://example.com": true,
+		"http://mock.mock": false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("Wanted %v, got %v", want, got)
+	}
+}
