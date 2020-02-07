837e2293a094c753878b372ebff7ba5cb66f97db
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-02-07 08:35:49 +0700

    Add integers section

---
+++ integers/adder_test.go
@@ -0,0 +1,21 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	got := Add(2, 3)
+	want := 5
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(2, 5)
+	fmt.Println(sum)
+	// Output: 7
+}
