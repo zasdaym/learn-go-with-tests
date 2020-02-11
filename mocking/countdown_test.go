5c746d715f2990078605e474e8d5e48684ca842c
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-02-11 17:06:56 +0700

    Clean up

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+const write = "write"
+const sleep = "sleep"
+
 func TestCountdown(t *testing.T) {
 
 	t.Run("prints 3 to Go!", func(t *testing.T) {
@@ -66,9 +69,6 @@ func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
 }
-
-const write = "write"
-const sleep = "sleep"
 
 type SpyTime struct {
 	durationSlept time.Duration
